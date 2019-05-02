6cd970f21704b5c8a00e7e27770978c7cb372be8
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-02 21:29:32 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,4 +1,15 @@
 def dictionary
+  words = {"hello" => "hi",
+  "to" => "2",
+  "two" => "2",
+  "too" => "2",
+  "for" => "4",
+  "four" => "4",
+  "be" => "b",
+  "you" => "u",
+  "at" => "@",
+  "and" => "&"
+  }
 end
 
 def word_subsituter
