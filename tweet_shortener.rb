79dad4fbeb01983c83d3c1d91de23eccc0252322
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-02 21:39:38 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,32 +1,50 @@
-def dictionary
-  words = {"hello" => "hi",
-  "to" => "2",
-  "two" => "2",
-  "too" => "2",
-  "for" => "4",
-  "four" => "4",
-  "be" => "b",
-  "you" => "u",
-  "at" => "@",
-  "and" => "&"
-  }
-end
+def word_substituter(tweet)
+  words = tweet.split(" ")
+  short_tweet = []
 
-def word_subsituter(tweets)
-  tweets.split(" ")
-  tweets.map do |tweet|
-    if tweet.keys.include?(word.downcase)
-      word = dictionary(word.downcase)
+  words.each do |word|
+    case word.downcase
+    when "hello"
+      short_tweet << "hi"
+    when "to", "two", "too"
+      short_tweet << "2"
+    when "for", "four" 
+      short_tweet << "4"
+    when "be" 
+      short_tweet << "b"
+    when "you" 
+      short_tweet << "u"
+    when "at" 
+      short_tweet << "@" 
+    when "and" 
+      short_tweet << "&"
+    else
+      short_tweet << word
     end
-  end.join(" ")
+  end
+  short_tweet.join(" ")
 end
 
-def bulk_tweet_shortener
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    tweet = word_substituter(tweet)
+    puts "#{tweet}"
+  end 
 end
 
-def selective_tweet_shortener
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
 end
 
-def shortened_tweet_truncator
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140 
+    tweet[0..136] + "..."
+  else
+    tweet
+  end
 end
-
