e66108161753d37a03d367ceb1daa57f46ee0b28
Reginald Jean Amedee <reginald.amedee@gmail.com>
2019-05-02 21:26:41 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def dictionary
+end
+
+def word_subsituter
+end
+
+def bulk_tweet_shortener
+end
+
+def selective_tweet_shortener
+end
+
+def shortened_tweet_truncator
+end
+
